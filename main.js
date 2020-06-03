7dcdf021a39307c006c2cc3795ce857d5e36987c
Dorota Pindur <dpindur@wp.pl>
2020-06-03 14:41:32 +0200

    date added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const allRanges = document.querySelectorAll(".range-wrapper");
 allRanges.forEach(wrap => {
   const range = wrap.querySelector(".range--js");
@@ -19,5 +21,23 @@ function setBubble(range, bubble) {
   // Sorta magic numbers based on size of the native UI thumb
   bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
+//end
 
+console.log(`It's working!`);
 
+var today = new Date();
+
+var day = today.getDate();
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+
+if (day < 10) {
+  day = '0' + day
+}
+if (month < 10) {
+  month = '0' + month
+}
+
+var out = document.getElementById("output");
+
+out.innerHTML = day + "/" + month + "/" + year;
